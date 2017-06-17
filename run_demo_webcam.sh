e6c51ccafe0b6641be63f8e7226c2af9b2c4d914
Carlos Gomes <kmartinho8@gmail.com>
2017-06-17 17:49:59 +0200

    moved one example to the source folder

--- run_demo_webcam.sh
+++ run_demo_webcam.sh
@@ -2,4 +2,4 @@
 
 
 echo "Going to start the real time demo with the webcam!"
-./Examples/Monocular/mono_main Vocabulary/ORBvoc.txt Examples/Monocular/webcam.yaml
+./bin/mono_main Vocabulary/ORBvoc.txt Vocabulary/webcam.yaml
